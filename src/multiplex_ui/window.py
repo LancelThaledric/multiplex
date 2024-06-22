80409ff3435700dc90d2115653b892434c3ad278
Valentin Mourot <thaledric@gmail.com>
2024-06-22 19:56:02 +0200

    feat: test 1st file using ffprobe

--- src/multiplex_ui/window.py
+++ src/multiplex_ui/window.py
@@ -1,3 +1,6 @@
+from PySide6 import QtWidgets
+from PySide6.QtCore import Slot, QProcess
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QMainWindow
 
 from src.ffmpeg_client.walker import FileWalker
@@ -8,10 +11,41 @@ type ProcessData = list
 
 class MPWindow(QMainWindow):
 
+    @Slot()
+    def on_process_message(self):
+        process = self.sender()
+        output = bytes(process.readAllStandardOutput()).decode('UTF-8').strip()
+        self.console.appendPlainText(output)
+
+    @Slot()
+    def on_process_error(self):
+        process = self.sender()
+        output = bytes(process.readAllStandardOutput()).decode('UTF-8').strip()
+        self.console.appendPlainText(output)
+
     def __init__(self, data: PickerData):
         super().__init__()
+
+        # State
         self.data = FileWalker(data)
-        print(self.data._input)
-        for f in self.data.iterfiles():
-            print([f])
+
+        # Test UI
+        self.console = QtWidgets.QPlainTextEdit()
+        self.console.setReadOnly(True)
+        monospace_font = QFont('Menlo')
+        monospace_font.setStyleHint(QFont.Monospace)
+        self.console.setFont(monospace_font)
+        self.setCentralWidget(self.console)
+        self.console.appendPlainText(' === MULTIPLEX === \n')
+
+        # Test Process
+        self.process = QProcess()
+        self.process.setProcessChannelMode(QProcess.ProcessChannelMode.MergedChannels)
+        self.process.readyReadStandardOutput.connect(self.on_process_message)
+        self.process.readyReadStandardError.connect(self.on_process_error)
+
+        files = self.data.iterfiles()
+        test_file = next(files)
+        self.process.start('ffprobe', ['-show_streams', '-of', 'json', str(test_file)])
+
         self.show()
